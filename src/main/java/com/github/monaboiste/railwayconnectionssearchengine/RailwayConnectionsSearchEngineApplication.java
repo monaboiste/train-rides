865b31599f6b5213e555a38a21d6c90df774a87a
monaboiste <monikaszumna4@gmail.com>
2023-03-06 20:50:40 +0100

    Init

---
+++ src/main/java/com/github/monaboiste/railwayconnectionssearchengine/RailwayConnectionsSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.github.monaboiste.railwayconnectionssearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayConnectionsSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayConnectionsSearchEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/monaboiste/railwayconnectionssearchengine/RailwayConnectionsSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.monaboiste.railwayconnectionssearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayConnectionsSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
